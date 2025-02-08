22612ae0abf992519dc0df3b891fc809ede9e484
TLS <thecolorizedsystem@gmail.com>
2025-02-08 02:10:44 +0200

    ahem, wtf FAPI

--- src/main/java/rings_of_saturn/github/io/galacticlib/GalacticLib.java
+++ src/main/java/rings_of_saturn/github/io/galacticlib/GalacticLib.java
@@ -1,10 +1,16 @@
 package rings_of_saturn.github.io.galacticlib;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.Item;
+
+import static rings_of_saturn.github.io.galacticlib.item.ItemGen.itemsToDatagen;
 
 public final class GalacticLib implements ModInitializer {
 
     @Override
     public void onInitialize() {
+        /*for (Item itemToDatagen : itemsToDatagen) {
+
+        }*/
     }
 }
--- src/main/java/rings_of_saturn/github/io/galacticlib/item/ItemGen.java
+++ src/main/java/rings_of_saturn/github/io/galacticlib/item/ItemGen.java
@@ -3,16 +3,26 @@ package rings_of_saturn.github.io.galacticlib.item;
 import net.minecraft.item.Item;
 import net.minecraft.registry.*;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import rings_of_saturn.github.io.galacticlib.client.GalacticLibClient;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Contains methods for generating items
+ */
 public final class ItemGen {
     public static ArrayList<Item> itemsToDatagen;
 
     public static Item createItem(String name, Item item, String MOD_ID){
-        return Registry.register(Registries.ITEM, Identifier.of(MOD_ID, name), item);
+        return Registry.register(Registries.ITEM, RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, name)).getValue(), item);
+    }
+    public static RegistryKey<Item> getItemKey(String name, String MOD_ID){
+        return RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, name));
+    }
+
+    public ItemGen() {
     }
 
     public static Item createDefaultModelItem(String name, Item item, String MOD_ID){
